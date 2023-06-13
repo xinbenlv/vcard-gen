b566f346d08feeac1fa62987fb391628b5287ece
Zainan Victor Zhou <zzn@zzn.im>
2023-06-12 23:16:38 -0400

    Add sample data

---
+++ README.md
@@ -0,0 +1,5 @@
+# vCard Generator
+
+## Attribution
+
+Using the avatar from [Flaticon](https://www.flaticon.com/free-icon/user_727399) by [Freepik][freepik] licensed under [CC 3.0 BY][cc3].
--- main.ts
+++ main.ts
@@ -1,29 +1,37 @@
 import vCardsJS from 'vcards-js';
 const fs = require('fs')
+const fg = require('fast-glob');
 
 
-function loadJSONFromFile(filePath: string): any {
-    const jsonString = fs.readFileSync(filePath, 'utf-8');
-    return JSON.parse(jsonString);
-}
-
-for (const name of ["VZ"]) {
-    const json = loadJSONFromFile(`dataset/${name}.json`);
-    // https://www.npmjs.com/package/vcards-js
-    const vCard = vCardsJS();
-    for (let key in json.vCard) {
-      if (key === "photo") {
-        vCard.photo.embedFromFile(json.vCard[key].embedFromFile);
-      } else if (key === "socialUrls") {
-        for (let urlKey in json.vCard[key]) {
-          vCard.socialUrls[urlKey] = json.vCard[key][urlKey];
-        }
-      } else {
-        vCard[key] = json.vCard[key];
-      }
+const main = async () => {
+    function loadJSONFromFile(filePath: string): any {
+        const jsonString = fs.readFileSync(filePath, 'utf-8');
+        return JSON.parse(jsonString);
     }
-
-    //save to file
-    vCard.saveToFile(`output/${name}.vcf`);
+    const jsonFiles = await fg(['dataset/*.json'], { dot: true });
     
+    for (const filename of jsonFiles) {
+        console.log(`Name XXX = `, filename);
+        const name = filename.split("/")[1].split(".")[0];
+        const json = loadJSONFromFile(filename);
+        // https://www.npmjs.com/package/vcards-js
+        const vCard = vCardsJS();
+        for (let key in json.vCard) {
+          if (key === "photo") {
+            vCard.photo.embedFromFile(json.vCard[key].embedFromFile);
+          } else if (key === "socialUrls") {
+            for (let urlKey in json.vCard[key]) {
+              vCard.socialUrls[urlKey] = json.vCard[key][urlKey];
+            }
+          } else {
+            vCard[key] = json.vCard[key];
+          }
+        }
+    
+        //save to file
+        vCard.saveToFile(`output/${name}.vcf`);
+        
+    }
 }
+
+main().then().catch(console.error);
